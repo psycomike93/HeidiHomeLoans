214ad8279e6e67722f09ddbfef2964ce9f959a8c
psycomike93 <psycomike93@gmail.com>
2025-07-22 08:43:43 -0500

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export const Navbar = () =>{
             <h2>Heidi Home Loans</h2>
             <a href= "#/" class = "navlink">Home</a>
             <a href= "#services" class = "navlink">Services</a>
-            <a href= "#about" class = "navlink">About Me</a>
+            <a href= "#about" class = "navlink">About</a>
             <a href= "#contact" class = "navlink">Contact</a>
         </nav>
 
